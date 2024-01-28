b909660c35fd3f19ef8a3cb7966ce601fe3977c4
puran <puransapkota4@gmail.com>
2024-01-29 00:10:08 +0545

    Learned about variable , comments and console.log

--- README.md
+++ README.md
@@ -29,3 +29,32 @@ This is all about git
 git add.
 git commit-m "added git status"
 git push
+
+Java script :
+javascript is the programming language of the web . Whatever activity we are doing on the web is controlled by javaz script
+
+What we will cover :
+
+1. Variables/Hoisting:
+   named storage for data
+
+keywords:
+Var : ES5/JS5 (Echma script version5 which is setup rules)
+Let: ES6
+Const: ES6
+
+declares variables to store data by using var ,let or const keywors
+
+EG:
+let ex="ram"
+var box ="red box"
+const pi= 3.14
+
+2. comments
+3. interactions
+4. Data Types
+5. operators
+6. Loop
+7. Functions
+8. Arrays
+9. Objects
---
+++ index.js
@@ -0,0 +1,30 @@
+//Prints the message
+console.log("Hello world");
+
+//variables
+//var,let,const
+
+//var(ES5)
+var broadwayTeacher = "raktim"; // always use camel case for joining more words for variables
+console.log(broadwayTeacher);
+
+// in var keywords, value can be reassigned. A container can only be defined once with the same name and data can be overrie.
+
+let teacher = "raktim";
+teacher = "sita";
+console.log(teacher);
+
+const teach = "raktim";
+console.log(teach);
+// The difference between var and let is var is ES5/JS5 and let is ES6
+//the difference between let and var is in let variable , value can still be reassigned and in const , vlaue cant be reaasigned even tho they are ES6
+
+// single line comment : //
+/* multi line comment */
+
+const table = "red table";
+const blackFan = "khaitan";
+const woodenChair = "furniture";
+const redPen = "mahakali";
+console.log(table, blackFan + woodenChair, redPen);
+console.log(2 + 2);
